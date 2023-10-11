9e51ed3db3774b8df4224f0505d3e6ea971f7a26
Gabriel Dias <GabrielDias7820@gmail.com>
2023-10-11 14:05:21 -0300

    adição do eslint padrão da rocketseat

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
 export function App() {
   return (
-    <h1> AAAAAAHAHAHHAHA </h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <h1>AAAAHAHAHAH</h1>
+    </ThemeProvider>
   )
 }
-
-
